1edf435e1dea39de7be62c2b4d0100ea441e7c28
mafintosh <mathiasbuus@gmail.com>
2012-11-19 01:09:46 +0100

    0.1.2

--- index.js
+++ index.js
@@ -1,31 +1,21 @@
-var net     = require('net');
+var net = require('net');
 var cluster = require('cluster');
-var parser  = require('./parser');
-var noop    = function() {};
+var rs = require('request-stream');
+var address = require('network-address');
+
+var noop = function() {};
 
 var TIMEOUT = 15*1000;
 
 var pools = {};
 
-var network = function() {
-	var faces = require('os').networkInterfaces();
-
-	for (var i in faces) {
-		for (var j = 0; j < faces[i].length; j++) {
-			if (faces[i][j].family === 'IPv4' && !faces[i][j].internal) {
-				return faces[i][j].address;
-			}
-		}
-	}
-	return '127.0.0.1';
-};
 var listen = function(port) {
 	var that = net.createServer();
 	var bind;
 
 	that.setMaxListeners(0);
 	that.once('listening', function() {
-		that.emit('bind', bind = network()+':'+that.address().port);
+		that.emit('bind', bind = address()+':'+that.address().port);
 	});
 	that.ready = function(fn) {
 		if (bind) return fn(bind);
@@ -41,76 +31,62 @@ var listen = function(port) {
 		cluster.isWorker = false;
 		that.listen(port);
 		process.env = env;
-		cluster.isWorker = true;		
+		cluster.isWorker = true;
 	}
 	return that;
 };
 var get = function(host) {
 	if (pools[host]) return pools[host];
 
-	var that = pools[host] = {};
 	var socket = net.connect(parseInt(host.split(':')[1], 10), host.split(':')[0]);
-	var onclose = function(err) {	
-		delete pools[host];
-		err = err || new Error('premature close');
-		Object.keys(that.callbacks).forEach(function(id) {
-			that.callbacks[id](err);
-		});
-		that.callbacks = {};
-	};
+	var r = pools[host] = pipe(socket);
 
-	that.socket = socket;
-	that.callbacks = {};
-	that.start = 0;
-	that.end = 0;
-	socket.on('close', onclose);
-	socket.on('error', onclose);
-	onsocket(socket, function(message) { // Protocol: [ID,MSG,ERR?]
-		var callback = that.callbacks[message[0]] || noop;
-		var err = message[2] && new Error(message[2]);
-
-		delete that.callbacks[message[0]];
-		callback(err, err ? undefined : message[1]);
-		if (++that.end !== that.start) return;
-		delete pools[host];
-		socket.destroy();
+	r.on('idle', function() {
+		console.log('idle')
+		socket.end();
 	});
-	return that;
+
+	r.on('end', function() {
+		delete pools[host];
+	});
+
+	return r;
 };
-var onsocket = function(socket, fn) {
+var pipe = function(socket) {
+	var r = rs();
+
 	socket.setTimeout(TIMEOUT, function() {
 		socket.destroy();
 	});
+
 	socket.on('end', function() {
-		socket.destroy();
+		socket.end();
 	});
-	socket.pipe(parser(fn));
+
+	socket.on('error', noop);
+
+	socket.pipe(r).pipe(socket);
+
+	return r;
 };
 var request = function(host, message, callback) {
-	var pool = get(host);
-
-	message = [pool.start++,message];
-	pool.callbacks[message[0]] = callback;
-	pool.socket.write(JSON.stringify(message)+'\n');
+	get(host).request(message, callback);
 };
 
 request.listen = function(port, onbind) {
 	if (typeof port === 'function') return request.listen(0, port);
 
 	var that = listen(port);
+	var onrequest = that.emit.bind(that, 'request');
 
 	that.on('connection', function(socket) {
-		onsocket(socket, function(message) { // Protocol: [ID,MSG,ERR?]
-			that.emit('request', message[1], function(err, res) {
-				message[1] = res;
-				if (err) message[2] = err.message;
-				socket.write(JSON.stringify(message)+'\n');
-			});
-		});
+		pipe(socket).on('request', onrequest);
 	});
+
 	if (onbind) {
 		that.once('bind', onbind);
 	}
+
 	return that;
 };
 
